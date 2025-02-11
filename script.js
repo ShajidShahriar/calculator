968597711d15db791f68698b2d44787e7dac0c78
ShajidShahriar <shajidsr12@gmail.com>
2025-02-11 21:29:16 +0600

    continuous operation corrected

--- script.js
+++ script.js
@@ -38,13 +38,16 @@ let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 let shouldResetDisplay = false;
+let singleOperation = false;
 
 const display = document.querySelector(".display");
 display.textContent = "0";
-const buttons = document.querySelectorAll(".buttons button, .zero-container button");
+const buttons = document.querySelectorAll(
+  ".buttons button, .zero-container button"
+);
 
 function updateDisplay(value) {
-  if (shouldResetDisplay) {
+  if (shouldResetDisplay || display.textContent ==="Error") {
     display.textContent = `${value}`;
     shouldResetDisplay = false;
   } else {
@@ -56,21 +59,56 @@ function updateDisplay(value) {
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const value = button.textContent;
-    if (!isNaN(value) || value === ".") {
+    if (!isNaN(value)) {
       updateDisplay(value);
-    } else if (["+", "-", "×", "÷"].includes(value)) {
+      return;
+    }
+
+    if (value === ".") {
+      if (display.textContent.includes(".")) return;
+      updateDisplay(value);
+      return;
+    }
+
+    if (["+", "-", "×", "÷"].includes(value)) {
       firstNumber = display.textContent;
       shouldResetDisplay = true;
+      singleOperation = true;
       operator = value;
-      console.log(value);
-      console.log(`firstnum: ${firstNumber}`);
-    } else if (value === "=") {
-      secondNumber = display.textContent;
-      console.log(`secondnum: ${secondNumber}`);
-      display.textContent = operate(firstNumber, secondNumber, operator);
-      console.log(`result:${display.textContent}`);
+
+      document.querySelectorAll(".operator").forEach((btn)=> btn.classList.remove("active-operator"))
+
+      button.classList.add("active-operator")
+      console.log(`firstnum:${firstNumber}`);
+      console.log(`secondnum:${secondNumber}`);
+      console.log(`singleoperation:${singleOperation}`);
+    }
+
+    if (value === "=") {
+      if (singleOperation) {
+        secondNumber = display.textContent;
+      } else {
+        firstNumber = display.textContent;
+      }
+
+      let result = operate(firstNumber, secondNumber, operator);
+      display.textContent = result;
+      console.log(`result:${result}`);
       shouldResetDisplay = true;
-    } else if (value === "±") {
+      singleOperation = false;
+      console.log(`firstnum:${firstNumber}`);
+      console.log(`secondnum:${secondNumber}`);
+      console.log(`singleoperation:${singleOperation}`);
+      document.querySelectorAll(".operator").forEach((btn)=> btn.classList.remove("active-operator"))
+
+      if (result === "Error") {
+        firstNumber = "";
+        operator = "";
+        secondNumber = "";
+      }
+    }
+
+    if (value === "±") {
       let currentValue = display.textContent;
 
       if (currentValue === "0") return;
@@ -79,15 +117,27 @@ buttons.forEach((button) => {
         display.textContent = currentValue.slice(1);
       } else {
         display.textContent = "-" + currentValue;
+        console.log(`firstnum:${firstNumber}`);
+        console.log(`secondnum:${secondNumber}`);
+        console.log(`singleoperation:${singleOperation}`);
       }
-    } else if (value === "C") {
+    }
+    if (value === "C") {
       shouldResetDisplay = true;
       updateDisplay("0");
       firstNumber = "";
       secondNumber = "";
-    } else if (value === "←") {
-      display.textContent = display.textContent.slice(0, -1) || "0";
-    }
+      console.log(`firstnum:${firstNumber}`);
+      console.log(`secondnum:${secondNumber}`);
+      console.log(`singleoperation:${singleOperation}`);
+      document.querySelectorAll(".operator").forEach((btn)=> btn.classList.remove("active-operator"))
 
+    }
+    if (value === "←") {
+      display.textContent = display.textContent.slice(0, -1) || "0";
+      console.log(`firstnum:${firstNumber}`);
+      console.log(`secondnum:${secondNumber}`);
+      console.log(`singleoperation:${singleOperation}`);
+    }
   });
 });
